a193fc7c1b09755a6d0e2addd0826c074850ff9b
John Goerzen <jgoerzen@complete.org>
2022-02-08 07:18:54 -0600

    CI incompatible with ftp

--- setup/setup.sh
+++ setup/setup.sh
@@ -5,7 +5,7 @@ set -x
 
 cd /tmp/setup
 wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multimedia-keyring_2016.8.1_all.deb
-wget ftp://ftp.osuosl.org/pub/mythtv/3rdParty/jwplayer.zip
+wget http://ftp.osuosl.org/pub/mythtv/3rdParty/jwplayer.zip
 sha256sum -c < sums
 dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
 echo 'deb http://deb.debian.org/debian bullseye-backports main contrib non-free' >> /etc/apt/sources.list
