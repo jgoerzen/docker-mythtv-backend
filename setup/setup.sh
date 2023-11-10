cbc52252028a1768c9ca88b090314873b2154106
John Goerzen <jgoerzen@complete.org>
2023-11-10 07:06:49 -0600

    bookworm

--- setup/setup.sh
+++ setup/setup.sh
@@ -10,8 +10,8 @@ wget http://www.deb-multimedia.org/pool/main/d/deb-multimedia-keyring/deb-multim
 wget http://ftp.osuosl.org/pub/mythtv/3rdParty/jwplayer.zip
 sha256sum -c < sums
 dpkg -i deb-multimedia-keyring_2016.8.1_all.deb
-echo 'deb http://deb.debian.org/debian bullseye-backports main contrib non-free' >> /etc/apt/sources.list
-echo 'deb http://www.deb-multimedia.org bullseye main non-free' >> /etc/apt/sources.list
-echo 'deb http://www.deb-multimedia.org bullseye-backports main' >> /etc/apt/sources.list
+echo 'deb http://deb.debian.org/debian bookworm-backports main contrib non-free' >> /etc/apt/sources.list
+echo 'deb http://www.deb-multimedia.org bookworm main non-free' >> /etc/apt/sources.list
+echo 'deb http://www.deb-multimedia.org bookworm-backports main' >> /etc/apt/sources.list
 rm deb-multimedia-keyring_2016.8.1_all.deb
 
